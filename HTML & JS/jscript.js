826cdef10b054349065052035eb65a799ab11f71
Farhan-120 <farxanshukri120@gmail.com>
2024-12-03 04:05:57 -0800

    waa personal website oo lugu dhisay html and tailwind

---
+++ HTML & JS/jscript.js
@@ -0,0 +1,71 @@
+// console.log('hellow wolrd ')
+// let x= 145
+// console.log(x)
+// let multiplication= 5*3;
+// console.log(multiplication);
+// let Subtracts= 5-5;
+// console.log(Subtracts)
+// let divition= 15/3;
+// console.log(divition)
+// let addintion= 4+4;
+// console.log(addintion)
+// let Subtract= 7-3;
+// console.log(Subtract)
+// let Equal=  2==2;
+// console.log(Equal)
+// var u= 'Haliimo';
+// let Abdi= '23'
+// const Muno= '67'
+// console.log(x)
+// console.log(Abdi)
+// console.log(Muno)
+// x= 12
+// t=26
+// console.log(x>t)
+// r= 'Abdi'
+// R='Abdi'
+// console.log(r>=R)
+// let number= '  I AM  23 years old '
+// let Ahmed = 'hellow every one My Name Abdihakiim, I Am 23 Years Old '
+// let year= ' I Was born 2004 '
+// let Nationality= 'From Somaliya,Speacialy Somaliland  ' 
+// let University= 'I Join JUDT University Last year '
+// console.log(Ahmed +year+ Nationality +University  )
+
+// let Abdi= 23;
+// let ahmed= 22;
+// console.log(Abdi != ahmed)
+
+// let istrue= !true
+// console.log(istrue)
+// let x= 8
+// let t=9
+// console.log(x-t)
+
+// function sum( num1, num2, num3, num4)
+// {   
+//  return num1+num2+num3+ num4;
+// }
+// console.log(sum(2,5,8,2));
+
+// let name='Abdihkiim Abdkariim '
+// function toUpper(text) {
+// return text.toUpperCase();
+
+// }
+// console.log(toUpper(name));
+// let Abdi='Sadam'
+// const toUpper= (text)=> {
+//     text.toUpperCase();
+// }
+
+// const age= 12
+// const massage= `my age is $`
+// console.log(`hellow{}`)
+ a= 15
+ b=10
+
+ a= 15-5
+ b= 10+5
+ console.log(a)
+ console.log(b)
